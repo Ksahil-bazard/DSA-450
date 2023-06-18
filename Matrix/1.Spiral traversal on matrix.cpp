aeeb6fba26fba03fe936b43d568ba3640baa7e1d
SAHIL BAZARD <89575121+Ksahil-bazard@users.noreply.github.com>
2023-06-18 18:15:08 +0530

    Update 1.Spiral traversal on matrix.cpp

--- Matrix/1.Spiral traversal on matrix.cpp
+++ Matrix/1.Spiral traversal on matrix.cpp
@@ -1,1 +1,51 @@
-
+vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
+    {
+        vector<int>ans;
+        int row=r;
+        int col =c;
+        int count =0;
+        int total =row*col;
+        
+        //// all index 
+        
+        int startingrow=0;
+        int startingcol=0;
+        int endingrow=row-1;
+        int endingcol=col-1;
+        
+        while(count<total)
+        {
+            for(int i=startingcol;count<total && i<=endingcol; i++)
+            {
+                ans.push_back(matrix[startingrow][i]);
+                count++;
+            }
+            startingrow++;
+            
+            for(int i=startingrow;count<total && i<=endingrow; i++)
+            {
+                ans.push_back(matrix[i][endingcol]);
+                count++;
+            }
+            endingcol--;
+            
+            for(int i=endingcol;count<total && i>=startingcol; i--)
+            {
+                ans.push_back(matrix[endingrow][i]);
+                count++;
+            }
+            endingrow--;
+            
+            for(int i=endingrow;count<total && i>=startingrow; i--)
+            {
+                ans.push_back(matrix[i][startingcol]);
+                count++;
+            }
+            startingcol++;
+        }
+        
+        
+        return ans;
+        
+        
+    }
